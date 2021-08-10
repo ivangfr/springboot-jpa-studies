fd4d6af4b09c4625c4d9cfc58855d574227e1ba6
Ivan Franchin <ivan.franchin@takeaway.com>
2021-08-10 18:34:32 +0200

    project update - update to spring-boot 2.5.3; - update to springdoc-openapi 1.5.10; - update to mysql docker image 8.0.26; - disable swagger petstore sample; - update .gitignore; - update README.

--- jpa-associations/src/test/java/com/mycompany/jpaassociations/AbstractTestcontainers.java
+++ jpa-associations/src/test/java/com/mycompany/jpaassociations/AbstractTestcontainers.java
@@ -14,7 +14,7 @@ public abstract class AbstractTestcontainers {
 
     private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.3");
 
-    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.25")
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.26")
             .withUrlParam("characterEncoding", "UTF-8")
             .withUrlParam("serverTimezone", "UTC");
 
