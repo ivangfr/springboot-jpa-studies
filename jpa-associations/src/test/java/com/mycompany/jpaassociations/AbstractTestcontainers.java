5e18abf21985d1871779521c092c80eebc5229cd
Ivan Franchin <ivan.franchin@takeaway.com>
2022-04-03 09:33:12 +0200

    project upgrade - upgrade to spring-boot 2.6.6; - upgrade to spring-cloud 2021.0.1; - upgrade to springdoc-openapi 1.6.6; - upgrade to guava 31.1-jre; - upgrade to postgres docker image 14.2.

--- jpa-associations/src/test/java/com/mycompany/jpaassociations/AbstractTestcontainers.java
+++ jpa-associations/src/test/java/com/mycompany/jpaassociations/AbstractTestcontainers.java
@@ -12,7 +12,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public abstract class AbstractTestcontainers {
 
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.1");
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:14.2");
 
     private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.28")
             .withUrlParam("characterEncoding", "UTF-8")
