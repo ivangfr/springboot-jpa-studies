da059c624c969081fc67db80ed75ffb3d94e5ea8
Ivan Franchin <ivan.franchin@takeaway.com>
2023-09-26 14:44:40 +0200

    Update docker image of mysql to 8.1.0 and postgres 15.4

--- jpa-batch/src/test/java/com/ivanfranchin/jpabatch/AbstractTestcontainers.java
+++ jpa-batch/src/test/java/com/ivanfranchin/jpabatch/AbstractTestcontainers.java
@@ -12,9 +12,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public abstract class AbstractTestcontainers {
 
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.3");
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.4");
 
-    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.33")
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0")
             .withUrlParam("characterEncoding", "UTF-8")
             .withUrlParam("serverTimezone", "UTC");
 
